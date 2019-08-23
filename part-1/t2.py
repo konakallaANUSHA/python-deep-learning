8f87827b8b158a63c8d306a823cf210e7256436c
Anusha Konakalla <akfch@mail.umkc.edu>
2019-08-23 17:49:57 -0500

    -

---
+++ part-1/t2.py
@@ -0,0 +1,44 @@
+x = 'python'
+y = (x.replace('on', ''))
+
+print(y)
+
+print(y[::-1])
+
+tr= input("enter a string: ")
+z=tr[:-2]
+print(z)
+print(z[::-1])
+
+
+
+t= input("enter a string: ")
+
+replaced_t = t.replace('python', 'pythons')
+
+
+print ('Original string:', t)
+
+print ('Replaced string:', replaced_t)
+
+num1String = input('Please enter an integer: ')
+num2String = input('Please enter a second integer: ')
+
+num1 = int(num1String)
+num2 = int(num2String)
+
+print ('Here is some output')
+
+
+#print num1,' plus ',num2,' equals ',num1+num2   --python2
+#print 'Thanks for playing'
+
+print (num1,' plus ',num2,' equals ',num1+num2)
+
+
+print (num1,' by ',num2,' equals ',num1/num2)
+
+print (num1,' mod ',num2,' equals ',num1 % num2)
+
+
+
