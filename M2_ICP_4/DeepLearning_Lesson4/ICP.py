6884c792380165b00556975c50e4ad947a996834
HACKTHON-FALL2019 <anushakonakalla2019gmail.com>
2019-11-15 18:23:28 -0600

    -

---
+++ M2_ICP_4/DeepLearning_Lesson4/ICP.py
@@ -0,0 +1,94 @@
+import numpy
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+import numpy as np
+from keras import backend as K
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+
+K.common.image_dim_ordering()
+
+tf.reset_default_graph()
+
+#tf.reset_default_graph()
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+'''plt.imshow(X_test[2])
+plt.show()'''
+# normalize inputs from 0-255 to 0.0-1.0
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+print(num_classes)
+# Create the model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compile model
+epochs = 50
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+tensorboard_callback = keras.callbacks.TensorBoard('./logs_icp')
+'''tbCallBack= keras.callbacks.TensorBoard('./logs', histogram_freq=1,
+                          write_graph=True,
+                          write_grads=True,
+                          batch_size=256,
+                         write_images=True)'''
+
+print(model.summary())
+# Fit the model
+model.fit(X_train,y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=1024 ,callbacks=[tensorboard_callback],)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+print(X_test.shape)
+#Predictions for first four images
+
+for i in range(0,4):
+    predicted_value = model.predict(X_test[[i],:])
+    predict_classes = model.predict_classes(X_test[[i],:])
+    actual_value = y_test[[i],:]
+    print("Actual Value for test image:" + str(i) + 'is ' + str(np.argmax(actual_value)))
+    print("Predicted Value for test image:" + str(i) + ' is' + str(predict_classes))
